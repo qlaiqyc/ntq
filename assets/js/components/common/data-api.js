51d2336590f36175fcef8a218c3090de0891b63e
qlaiqyc <qyc123abc>
2017-05-05 18:02:03 +0800

    ces

--- assets/js/components/common/data-api.js
+++ assets/js/components/common/data-api.js
@@ -1,174 +1,210 @@
-PageInfo.register({"type":"plug","info":(function(){
- 
- 
-	var FunUtil = {};
-	
-	FunUtil.common4init = function(){
- 		
- 		String.HasText = function(str) {
-			try{
-			
-			if (typeof(str) == "undefined") return false;
-			if (str == null) return false;
-			if (str == 'null') return false;
-			if (str == 'undefined') return false;
-		
-			if (typeof(str) == 'string')
-				str = str.replace(/(^\s*)|(\s*$)/g, '');
-			if (str === '') return false;
-			
-			}catch(e){
-				return false;
-			}
-			return true;
+PageInfo.register({
+	"type": "plug",
+	"info": (function() {
+
+		var FunUtil = {};
+
+		FunUtil.common4init = function() {
+
+			String.HasText = function(str) {
+				try {
+
+					if(typeof(str) == "undefined") return false;
+					if(str == null) return false;
+					if(str == 'null') return false;
+					if(str == 'undefined') return false;
+
+					if(typeof(str) == 'string')
+						str = str.replace(/(^\s*)|(\s*$)/g, '');
+					if(str === '') return false;
+
+				} catch(e) {
+					return false;
+				}
+				return true;
+			};
 		};
- 	};
-	
-	FunUtil.request = function(data){
-		FunUtil.common4init();
-	 	var execuFun ={};
-	 
-		execuFun.pub = function(){
-			
-			/*原理类似只需在options 添加参数进行判断 */
-			var contentType = (data.method == "post") ? "application/x-www-form-urlencoded" :"application/json";
-			 
-			contentType= "application/json";
-		    if(!String.HasText(data.method)) return;
-	        
-	         $.ajax({
-	             type:data.method,
-	             url:data.url,
-	             data: data.param,
-	             dataType: "json",
-	             contentType:contentType,
-	            success: function(cdata) {
-	                 
-	                data.success(cdata);
-	            },
-	            error: function(data) {
-	                
-	            }
-	        });
-	                        
-			
-			
-			
+
+		FunUtil.request = function(data) {
+			FunUtil.common4init();
+			var execuFun = {};
+
+			execuFun.pub = function() {
+
+				/*原理类似只需在options 添加参数进行判断 */
+				var contentType = (data.method == "post") ? "application/x-www-form-urlencoded" : "application/json";
+
+				contentType = "application/json";
+				if(!String.HasText(data.method)) return;
+
+				$.ajax({
+					type: data.method,
+					url: data.url,
+					data: data.param,
+					dataType: "json",
+					crossDomain:true,
+					contentType: contentType,
+					success: function(cdata) {
+
+						data.success(cdata);
+					},
+					error: function(data) {
+
+					}
+				});
+
+			};
+
+			/* 
+			 *  ==== 必填
+			 * method: 执行方法 get post delete put
+			 * url: 参数地址
+			 * success:成功回调
+			 * param: 传递参数
+			 * 
+			 *  ==== 可填可不填
+			 * fail:失败回调
+			 * async: 默认 true
+			 * 
+			 *FunUtil.request({"url","method":"put","param":{},"success":function(){
+			 *}})
+			 * 
+			 * */
+			execuFun.pub();
+
 		};
-	 
-		
-		/* 
-		 *  ==== 必填
-		 * method: 执行方法 get post delete put
-		 * url: 参数地址
-		 * success:成功回调
-		 * param: 传递参数
-		 * 
-		 *  ==== 可填可不填
-		 * fail:失败回调
-		 * async: 默认 true
-		 * 
-		 *FunUtil.request({"url","method":"put","param":{},"success":function(){
-		 *}})
-		 * 
-		 * */
-		execuFun.pub();
-		
-		
-	};
-	
-	
-	/*请求集合 */
-	
-	FunUtil.common4post = function(data,callback){
-		FunUtil.request({"method":"post","url":data.url,"param":data.param,"success":callback});
-	};
-	
-	
-	FunUtil.common4get = function(data,callback){
-		
-		FunUtil.request({"method":"get","url":data.url,"param":data.param,"success":callback});
-		
-	}
-	
-	
-	FunUtil.rq = function(data, success){
-		
-		
+
+		/*请求集合 */
+
+		FunUtil.common4post = function(data, callback) {
+			FunUtil.request({
+				"method": "post",
+				"url": data.url,
+				"param": data.param,
+				"success": callback
+			});
+		};
+
+		FunUtil.common4get = function(data, callback) {
+
+			FunUtil.request({
+				"method": "get",
+				"url": data.url,
+				"param": data.param,
+				"success": callback
+			});
+
+		}
+
+		FunUtil.rq = function(data, success) {
+
 			var burl = "http://119.23.69.169:8080/ntq/";
-		
-		return {
-			
-			   /*=======招聘模块============ */
-			 	'getMessageCode': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/getMessageCode.action"),"param":data}, success);
-            	},
-            	'verifyMessageCode': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/verifyMessageCode.action"),"param":data}, success);
-            	},
-            	'getMessageAfterValidatePhoneNumber': function(data, success) {
-               		 FunUtil.common4post({url:(burl + "companyInfo/getMessageAfterValidatePhoneNumber.action"),"param":data}, success);
-            	},
-            	
-            	'verifyHrPhoneNumber': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/verifyHrPhoneNumber.action"),"param":data}, success);
-            	},
-            	
-            	'addCompanyInfoWithPositionInfoList': function(data, success) {
-               		 FunUtil.common4post({url:(burl + "companyInfo/addCompanyInfoWithPositionInfoList.action"),"param":data}, success);
-            	},
-            	
-            	'addCompanyPositionInfo': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/addCompanyPositionInfo.action"),"param":data}, success);
-            	},
-            	
-            	'queryCompanyPositionInfoListByCondition': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/queryCompanyPositionInfoListByCondition.action"),"param":data}, success);
-            	},
-            	
-            	'getCompanyPositionInfoById': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/getCompanyPositionInfoById.action"),"param":data}, success);
-            	},
-            	
-            	'queryJobSeekerInfoVoList': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/queryJobSeekerInfoVoList.action"),"param":data}, success);
-            	},
-            	
-            	'updateOrInsertCompanyPositionInfo': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/updateOrInsertCompanyPositionInfo.action"),"param":data}, success);
-            	},
-            	
-            	'withDrawCompanyPositionInfo': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/withDrawCompanyPositionInfo.action"),"param":data}, success);
-            	},
-			
-			   /*=======求职者模块============== */
-			  
-			    'getInfos': function(data, success) {
-                	FunUtil.common4post({url:(burl + "get/position/getinfos.action"),"param":data}, success);
-            	},
-            	
-            	'addJobSeekerInfoAndResume': function(data, success) {
-                	FunUtil.common4post({url:(burl + "jobSeekerInfo/addJobSeekerInfoAndResume.action"),"param":data}, success);
-            	},
-            	'getPosition': function(data, success) {
-                	FunUtil.common4post({url:(burl + "companyInfo/getPosition.action"),"param":data}, success);
-            	},
-            	 
-			  
-			
+
+			return {
+
+				/*=======招聘模块============ */
+				'getMessageCode': function(data, success) {
+					FunUtil.common4get({
+						url: (burl + "companyInfo/getMessageCode.action"),
+						"param": data
+					}, success);
+				},
+				'verifyMessageCode': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/verifyMessageCode.action"),
+						"param": data
+					}, success);
+				},
+				'getMessageAfterValidatePhoneNumber': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/getMessageAfterValidatePhoneNumber.action"),
+						"param": data
+					}, success);
+				},
+
+				'verifyHrPhoneNumber': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/verifyHrPhoneNumber.action"),
+						"param": data
+					}, success);
+				},
+
+				'addCompanyInfoWithPositionInfoList': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/addCompanyInfoWithPositionInfoList.action"),
+						"param": data
+					}, success);
+				},
+
+				'addCompanyPositionInfo': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/addCompanyPositionInfo.action"),
+						"param": data
+					}, success);
+				},
+
+				'queryCompanyPositionInfoListByCondition': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/queryCompanyPositionInfoListByCondition.action"),
+						"param": data
+					}, success);
+				},
+
+				'getCompanyPositionInfoById': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/getCompanyPositionInfoById.action"),
+						"param": data
+					}, success);
+				},
+
+				'queryJobSeekerInfoVoList': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/queryJobSeekerInfoVoList.action"),
+						"param": data
+					}, success);
+				},
+
+				'updateOrInsertCompanyPositionInfo': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/updateOrInsertCompanyPositionInfo.action"),
+						"param": data
+					}, success);
+				},
+
+				'withDrawCompanyPositionInfo': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/withDrawCompanyPositionInfo.action"),
+						"param": data
+					}, success);
+				},
+
+				/*=======求职者模块============== */
+
+				'getInfos': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "get/position/getinfos.action"),
+						"param": data
+					}, success);
+				},
+
+				'addJobSeekerInfoAndResume': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "jobSeekerInfo/addJobSeekerInfoAndResume.action"),
+						"param": data
+					}, success);
+				},
+				'getPosition': function(data, success) {
+					FunUtil.common4post({
+						url: (burl + "companyInfo/getPosition.action"),
+						"param": data
+					}, success);
+				},
+
+			};
+
 		};
-		
-		
-		
-	};
-	
- 	 
- 
- 
- 
- 	
- 	return FunUtil
- 
- 
-})()});
+
+		return FunUtil
+
+	})()
+});
--- assets/js/components/employ/index.js
+++ assets/js/components/employ/index.js
@@ -1,102 +1,117 @@
-PageInfo.register({"type":"Obj","info":function(){
-	
-	var PageObj = {};
-	 
-	PageObj.require = {
-		"$":"jquery",
-		"layui":"layui"
-	};
-	
-	 
-	 PageObj.page = function(){
-		
-		var FunUtil = {};
-	    var HtmUtil = {};
-    	var Page 	= {};
-	
-		HtmUtil.common4input   = function(){
-			
-			var buf = [];
-		 	 
-			buf.push('<form class="layui-form" action="" id="ntq-employ-index-form">')
-			buf.push('    <div class="layui-form-item">')
-			buf.push('        <div class="layui-inline">')
-			buf.push('            <label class="layui-form-label">验证手机</label>')
-			buf.push('            <div class="layui-input-inline">')
-			buf.push('                <input type="tel" name="phone" lay-verify="phone" autocomplete="off" class="layui-input">')
-			buf.push('            </div>')
-			buf.push('            <label class="layui-form-label" id="ntq-employ-index-form-code">获取验证码</label>')
-			buf.push('        </div>')
-			buf.push('    </div>')
-			buf.push('    <div class="layui-form-item">')
-			buf.push('        <div class="layui-inline">')
-			buf.push('            <label class="layui-form-label">输入验证码</label>')
-			buf.push('            <div class="layui-input-inline">')
-			buf.push('                <input type="text" name="number" lay-verify="required|number" autocomplete="off" class="layui-input">')
-			buf.push('            </div>')
-			buf.push('        </div>')
-			buf.push('    </div>')
-			buf.push('    <div class="layui-form-item">')
-			buf.push('        <div class="layui-input-block">')
-			buf.push('            <button id="ntq-employ-index-form-btn" class="layui-btn" lay-submit="" lay-filter="demo1">立即提交</button>')
-			buf.push('            <button type="reset" class="layui-btn layui-btn-primary">重置</button>')
-			buf.push('        </div>')
-			buf.push('    </div>')
-			buf.push('</form>')
-			
-			return buf.join("");
-			
-		};
-	
-		
-		Page.show = function(){
-			 
-			var $no = $("#ntq-employ-index-btn-no");
-			var $has = $("#ntq-employ-index-btn-has");
-			
-			$no.unbind("click").bind("click",function(){
-				PageInfo.FunUtil.common4openUrl({"url":'employ/add'});
+PageInfo.register({
+			"type": "Obj",
+			"info": function() {
+
+				var PageObj = {};
+
+				PageObj.require = {
+					"$": "jquery",
+					"layui": "layui",
+					"api": "/assets/js/components/common/data-api"
+				};
+
+				PageObj.page = function() {
+
+					var FunUtil = {};
+					var HtmUtil = {};
+					var Page = {};
+
+					HtmUtil.common4input = function() {
+
+						var buf = [];
+
+						buf.push('<form class="layui-form" action="" id="ntq-employ-index-form">')
+						buf.push('    <div class="layui-form-item">')
+						buf.push('        <div class="layui-inline">')
+						buf.push('            <label class="layui-form-label">验证手机</label>')
+						buf.push('            <div class="layui-input-inline">')
+						buf.push('                <input type="tel" name="phone" lay-verify="phone" autocomplete="off" class="layui-input" id="ntq-employ-index-form-phone">')
+						buf.push('            </div>')
+						buf.push('            <label class="layui-form-label" id="ntq-employ-index-form-code">获取验证码</label>')
+						buf.push('        </div>')
+						buf.push('    </div>')
+						buf.push('    <div class="layui-form-item">')
+						buf.push('        <div class="layui-inline">')
+						buf.push('            <label class="layui-form-label">输入验证码</label>')
+						buf.push('            <div class="layui-input-inline">')
+						buf.push('                <input type="text" name="number" lay-verify="required|number" autocomplete="off" class="layui-input">')
+						buf.push('            </div>')
+						buf.push('        </div>')
+						buf.push('    </div>')
+						buf.push('    <div class="layui-form-item">')
+						buf.push('        <div class="layui-input-block">')
+						buf.push('            <button   class="layui-btn" lay-submit="" lay-filter="ntq-employ-index-form-btn">立即提交</button>')
+						buf.push('            <button type="reset" class="layui-btn layui-btn-primary">重置</button>')
+						buf.push('        </div>')
+						buf.push('    </div>')
+						buf.push('</form>')
+
+						return buf.join("");
+
+					};
+
+					Page.show = function() {
+						var request = this.api.rq();
+						var $no = $("#ntq-employ-index-btn-no");
+						var $has = $("#ntq-employ-index-btn-has");
+
+						$no.unbind("click").bind("click", function() {
+							PageInfo.FunUtil.common4openUrl({"url": 'employ/add' });
+						});
+
+						$has.unbind("click").bind("click", function() {
+
+								layui.use(['form', 'layedit', 'laydate'], function() {
+										var form = layui.form(),
+											layer = layui.layer,
+											layedit = layui.layedit,
+											laydate = layui.laydate;
+
+										layer.open({
+
+												type: 1,
+												skin: 'layui-layer-demo',
+												area: ['420px', '240px'], //宽高
+												content: HtmUtil.common4input(),
+												success: function() {
+													/*$("#ntq-employ-index-form-btn").unbind("click").bind("click",function(){
+														layer.closeAll()
+														PageInfo.FunUtil.common4openUrl({"url":'employ/job'});
+													});*/
+													
+													var $phone = $("#ntq-employ-index-form-phone");
+													
+
+													$("#ntq-employ-index-form-code").unbind("click").bind("click", function() {
+															var phoneNumber = $phone.val();
+															
+															if(!String.HasText(phoneNumber)) return;
+															
+															request.getMessageCode({"phoneNumber":phoneNumber},function(data){
+																console.log(data);
+															});
+													});
+
+													form.on('submit(ntq-employ-index-form-btn)', function(data) {
+
+															return false;
+													 
+													});
+												}
+												
+										});		
+											
+										form.render('');
+
+										});
+
+								});
+						};
+
+						return Page;
+
+					};
+
+					return PageObj;
+				}
 			});
-			
-			$has.unbind("click").bind("click",function(){
-				  
-			      
-			      
-			      layui.use(['form', 'layedit', 'laydate'], function(){
-					    var form = layui.form()
-					    ,layer = layui.layer
-					    ,layedit = layui.layedit
-					    ,laydate = layui.laydate;
-					    
-					    layer.open({
-			            
-				          type: 1,
-						   skin: 'layui-layer-demo',
-						   area: ['420px', '240px'], //宽高
-						   content: HtmUtil.common4input(),
-						   success:function(){
-						   	$("#ntq-employ-index-form-btn").unbind("click").bind("click",function(){
-						   		layer.closeAll()
-						   		PageInfo.FunUtil.common4openUrl({"url":'employ/job'});
-						   	});
-						   }
-				        });
-					  
-					 	form.render('');
-					      
-			      });
-			      
-			      
-			});
-		};
-		
-		return Page;
-		
-	};
-	
-	
-	return PageObj;
-}});
-
-
-
