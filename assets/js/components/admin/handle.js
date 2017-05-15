00459e9d56e3648b42136d39965f7efd98e223f5
qlaiqyc <qyc123abc>
2017-05-15 16:24:01 +0800

    测试

--- assets/js/components/admin/handle.js
+++ assets/js/components/admin/handle.js
@@ -1,77 +1,38 @@
 PageInfo.register({"type":"Obj","info":function(){
-	var FunUtil = {};
-    var HtmUtil = {};
-    var PageObj = {};
 	
-	
-	FunUtil.Global = {
-		"child":{
-			"id":123
-		}
-	};//传参对象
-	
+	var PageObj = {};
+	 
 	PageObj.require = {
-		"$":"jquery",
-		"layui":"layui"
+		"$": "jquery",
+		"layui": "layui",
+		"api": "/assets/js/components/common/data-api"
 	};
 	
-	PageObj.page = {
-			    data(){
-			        var param = {};
-			        
-			        param.HtmUtil = HtmUtil;
-			        param.FunUtil = FunUtil;
-			        
-			        return param;
-			    },
-			    init(){
-			         
-			    },
-			    update(){
-			        
-			    },
-			    show(){
-			    	 
-			    	var $this = this;
-			    	
-			    	 
-			    	layui.use(['form', 'layedit', 'laydate'], function(){
-					  var form = layui.form()
-					  ,layer = layui.layer
-					  ,layedit = layui.layedit
-					  ,laydate = layui.laydate,
-					 	laypage = layui.laypage
-	  				  ,layer = layui.layer;
-					 	form.render('');
-					 	
-					 	laypage({
-							cont: 'ntq-admin-index-condition-pag',
-							pages: 100,
-							skip: true
-						});
-					  //创建一个编辑器
-					  var $ = layui.jquery, form = layui.form();
-					  
-					  //全选
-					  form.on('checkbox(allChoose)', function(data){
-					    var child = $(data.elem).parents('table').find('tbody input[type="checkbox"]');
-					    child.each(function(index, item){
-					      item.checked = data.elem.checked;
-					    });
-					    form.render('checkbox');
-					  });
-					  
-					});
-			    },
-			    hide(){
-			    	//console.log("==add==hide")
-			    },   
-			    destory(){
-			        
-			    }
+	 
+	PageObj.page = function(){
+		
+		var FunUtil = {};
+	    var HtmUtil = {};
+    	var Page 	= {};
+	
+		Page.show = function(){
+			 var request = this.api.rq();
+			 
+			 layui.use(['form', 'layedit', 'laydate','laypage', 'layer'], function(){ var form = layui.form() ,layer = layui.layer ,layedit = layui.layedit ,laydate = layui.laydate, laypage = layui.laypage ,layer = layui.layer;
+			 	form.render(''); 
+			 	
+		 
+			 });
 			
-			};
+		 
+		};
+		
+		return Page;
+		
+	};
+	
 	
 	return PageObj;
 }});
+
 
